ab94cd8b6e6a0658477e443985188a4e5c550e44
linuber <linuber@student.chalmers.se>
2024-03-24 17:17:29 +0100

    more level

--- Player_test.gd
+++ Player_test.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const ACCELERATION = 1000.0
+const COINCELERATION = 10.0 
 const DECELERATION = 500.0
+const DECOINCELERATION = 5.0
 const JUMP_VELOCITY = -500.0
 var jump_sound
 var pickup_sound
@@ -29,17 +31,17 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY-coins
 		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction*SPEED, ACCELERATION*delta)
+		velocity.x = move_toward(velocity.x, direction*SPEED, (ACCELERATION-coins*COINCELERATION)*delta)
 	else:
 
-		velocity.x = move_toward(velocity.x, 0, DECELERATION*delta)
+		velocity.x = move_toward(velocity.x, 0, (DECELERATION-coins*DECOINCELERATION)*delta)
 
 	if position.y > min:
 		game_over()
