07b07925715d2afdc948c8a31184fb5175d7fd2d
Evelina Frännhag <evelinafrannhag@gmail.com>
2024-03-24 17:16:08 +0100

    Improve game

--- Player_test.gd
+++ Player_test.gd
@@ -8,7 +8,9 @@ const JUMP_VELOCITY = -500.0
 var jump_sound
 var pickup_sound
 var min = 500
+var max = 1000
 var coins = 0
+var time = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -23,6 +25,7 @@ func pickup():
 	pickup_sound.play()
 
 func _physics_process(delta):
+	time += delta
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -43,9 +46,18 @@ func _physics_process(delta):
 
 	if position.y > min:
 		game_over()
+	if position.x > max:
+		game_win()
 
 
 	move_and_slide()
 
+func game_win():
+	pass
+
 func game_over():
+	print(coins)
+	print(time)
+	coins = 0
+	time = 0
 	get_tree().reload_current_scene()
